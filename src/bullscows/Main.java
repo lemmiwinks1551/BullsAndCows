084de0ac4cfeae5ec7c8ac402773be43f2f11e73
kirill <ananevka@mail.ru>
2022-03-21 23:27:56 +0300

    stage 2

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -1,18 +1,49 @@
 package bullscows;
 
+import java.util.Random;
+import java.util.Scanner;
+
+// 9305
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("""
-                The secret code is prepared: ****.
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String[] randomNum = String.valueOf("9305").split(""); // random.nextInt(10_000)
+        String[] guessNum = scanner.nextLine().split("");
 
-                Turn 1. Answer:
-                1234
-                Grade: None.
+        int bulls = 0, cows = 0;
 
-                Turn 2. Answer:
-                9876
-                Grade: 4 bulls.
-                Congrats! The secret code is 9876.""");
+        for (int i = 0; i < guessNum.length; i++) {
+            if (guessNum[i].equals(randomNum[i])) {
+                bulls++;
+            }
+        }
+
+        for (int i = 0; i < guessNum.length; i++) {
+            for (int j = 0; j < randomNum.length; j++) {
+                if (guessNum[i].equals(randomNum[j]) && i != j) {
+                    cows++;
+                }
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (String value : randomNum) {
+            builder.append(value);
+        }
+        String randomNumOutput = builder.toString();
+
+        if (cows == 0 && bulls == 0) {
+            System.out.printf("Grade: None. The secret code is %s.", randomNumOutput);
+        } else if (bulls == 0) {
+            System.out.printf("Grade: %d cow(s). The secret code is %s.", cows, randomNumOutput);
+        } else if (cows == 0) {
+            System.out.printf("Grade: %d bull(s). The secret code is %s.", bulls, randomNumOutput);
+        } else if (bulls > 0 && cows > 0) {
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", bulls, cows, randomNumOutput);
+        }
+
     }
 }
