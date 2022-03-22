38b00ddb60629879723ad37d3826b618d8fbf243
kirill <ananevka@mail.ru>
2022-03-22 22:51:55 +0300

    stage 4

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -4,52 +4,76 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final String outputErrorLength = "Error: can't generate a secret number with a length of %d because there aren't enough unique digits.";
-    public static final String  outputRndNum = "The random secret number is %s.";
+    public static final String outputErrorLength = "Error: can't generate a secret number with a length of %d because there aren't enough unique digits.\n";
+    public static final String outputRndNum = "The random secret number is %s.";
+    public static final String outputGameStart1 = "Please, enter the secret code's length:";
+    public static final String outputGameStart2 = "Okay, let's start a game!";
+    public static final String outputGameEnd = "Congratulations! You guessed the secret code.";
+    public static final String outputTurn = "Turn %d:\n";
+
+    public static final String outputNoBullsNoCows = "Cannot find number of bulls or number of cows or None after the input.\n";
+    public static final String noBulls = "Grade: %d cows\n";
+    public static final String noCows = "Grade: %d bulls\n";
+    public static final String haveBullsAndCows = "Grade: %d bull and %d cow\n";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] guessNum;
+
+        System.out.println(outputGameStart1);
         int length = scanner.nextInt();
 
-        StringBuilder sb = new StringBuilder(generatePseudoRandomNumber(length));
+        StringBuilder randomNum = new StringBuilder(generatePseudoRandomNumber(length));
 
-        System.out.printf(outputRndNum, sb);
-        /*
-        Random random = new Random();
-        String[] randomNum = String.valueOf("9305").split(""); // random.nextInt(10_000)
-        String[] guessNum = scanner.nextLine().split("");
+        System.out.println(outputGameStart2);
+        int bulls = 0, cows = 0, turn = 1;
 
-        int bulls = 0, cows = 0;
+        guessNum = scanner.nextLine().split("");
 
+        while (true) {
+            System.out.printf(outputTurn, turn);
+            guessNum = scanner.nextLine().split("");
+            bulls = bullsCount(guessNum, randomNum);
+            cows = cowsCount(guessNum, randomNum);
+
+            if (cows == 0 && bulls == 0) {
+                System.out.print(outputNoBullsNoCows);
+            } else if (bulls == 0) {
+                System.out.printf(noBulls, cows);
+            } else if (cows == 0 && bulls != length) {
+                System.out.printf(noCows, bulls);
+            } else if (bulls > 0 && cows > 0) {
+                System.out.printf(haveBullsAndCows, bulls, cows);
+            } else if (bulls == length) {
+                System.out.printf(noCows, bulls);
+                System.out.print(outputGameEnd);
+                break;
+            }
+            turn++;
+        }
+
+    }
+
+    private static int bullsCount (String[] guessNum, StringBuilder randomNum) {
+        int bulls = 0;
         for (int i = 0; i < guessNum.length; i++) {
-            if (guessNum[i].equals(randomNum[i])) {
+            if (guessNum[i].equals(randomNum.toString().split("")[i])) {
                 bulls++;
             }
         }
+        return bulls;
+    }
 
+    private static int cowsCount (String[] guessNum, StringBuilder randomNum) {
+        int cows = 0;
         for (int i = 0; i < guessNum.length; i++) {
-            for (int j = 0; j < randomNum.length; j++) {
-                if (guessNum[i].equals(randomNum[j]) && i != j) {
+            for (int j = 0; j < randomNum.length(); j++) {
+                if (guessNum[i].equals(randomNum.toString().split("")[j]) && i != j) {
                     cows++;
                 }
             }
         }
-
-        StringBuilder builder = new StringBuilder();
-        for (String value : randomNum) {
-            builder.append(value);
-        }
-        String randomNumOutput = builder.toString();
-
-        if (cows == 0 && bulls == 0) {
-            System.out.printf("Grade: None. The secret code is %s.", randomNumOutput);
-        } else if (bulls == 0) {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.", cows, randomNumOutput);
-        } else if (cows == 0) {
-            System.out.printf("Grade: %d bull(s). The secret code is %s.", bulls, randomNumOutput);
-        } else if (bulls > 0 && cows > 0) {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", bulls, cows, randomNumOutput);
-        }*/
+        return cows;
     }
 
     private static StringBuilder generatePseudoRandomNumber (int length) {
@@ -72,3 +96,25 @@ public class Main {
         return sb;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
